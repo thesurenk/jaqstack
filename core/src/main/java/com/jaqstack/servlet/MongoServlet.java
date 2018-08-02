ff123e3cf9fd636e440ba081d1d6da38660c0438
Suren Konathala <konathalasuren@gmail.com>
2018-08-02 16:53:16 -0500

    Included Mongo sample servlet with REST service

---
+++ core/src/main/java/com/jaqstack/servlet/MongoServlet.java
@@ -0,0 +1,32 @@
+package com.jaqstack.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.jaqstack.data.MongoExample;
+
+@Path("/dbservice")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class MongoServlet {
+
+    @GET
+    @Path("/product")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getProduct() throws ServletException {
+        MongoExample me = new MongoExample();
+
+        return me.getProduct();
+    }
+
+}
