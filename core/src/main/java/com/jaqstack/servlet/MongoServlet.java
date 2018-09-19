d9161e0e5a9ed3f268b41d048a19011d108fd6ec
Suren Konathala <konathalasuren@gmail.com>
2018-09-19 14:18:51 -0500

    Updates to package, new ui folder dopetrope

--- core/src/main/java/com/jaqstack/servlet/MongoServlet.java
+++ core/src/main/java/com/jaqstack/servlet/MongoServlet.java
@@ -9,7 +9,6 @@ import java.io.PrintWriter;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
